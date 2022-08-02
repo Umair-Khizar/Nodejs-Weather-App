10c0cf469c1f0874e93c9c888c0284a8dadf34cb
Umair Bhatti <bhattiumair596@gmail.com>
2022-08-02 01:08:20 -0700

    Changes Some Code

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,18 +1,25 @@
-const request = require('request')
+const request = require('request');
+console.log("Geocode here....");
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    // const address = "Sialkot";
+    // const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'http://api.positionstack.com/v1/forward?access_key=c06d40dede8fd4a6b16e5736bec55918&query=' + encodeURIComponent(address);
 
     request({ url, json: true }, (error, { body }) => {
+        // console.log(body.length);
+        console.log(body.data.length);
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
+
+        } 
+        else if (body.data.length === 0,undefined) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
-                latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
-                location: body.features[0].place_name
+                latitude: body.data[0].latitude,
+                longitude: body.data[0].longitude,
+                location: body.data[0].label
             })
         }
     })
